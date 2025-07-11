60cc93fb322aa1af83497b233be1ee9fd262f11a
asatullayev-diyorbek <asatullayevdiyorbek3@gmail.com>
2025-07-11 05:53:23 +0500

    First commit

--- bot/models.py
+++ bot/models.py
@@ -16,7 +16,7 @@ class BotUser(models.Model):
     full_name = models.CharField(max_length=500)
     phone = models.CharField(max_length=20, unique=True, null=True, blank=True)
     cash = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    status  = models.BooleanField(default=False)
+    status  = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
